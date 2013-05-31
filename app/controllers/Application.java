69a5bf68e1055a2724e649d3848663d1f6db5b00
Harald Wartig <hwartig@gmail.com>
2013-05-31 07:02:24 +0200

    Improve form

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,23 @@
 package controllers;
 
+import play.data.DynamicForm;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static play.data.DynamicForm.form;
+
 public class Application extends Controller {
-  
     public static Result index() {
-        return ok(index.render());
+        DynamicForm requestData = form().bindFromRequest();
+        BigDecimal amount = new BigDecimal(requestData.get("amount"));
+        Currency fromCurrency = Currency.getInstance(requestData.get("from"));
+        Currency toCurrency = Currency.getInstance(requestData.get("to"));
+        BigDecimal result = amount;
+
+        return ok(index.render(fromCurrency, amount, toCurrency, result));
     }
 }
--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,24 +1,29 @@
 import org.junit.Test;
 import play.mvc.Content;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.contentAsString;
 import static play.test.Helpers.contentType;
 
 
 public class ApplicationTest {
-
     @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
-    }
-
-    @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render();
+    public void renderTemplateWithoutValues() {
+        Content html = views.html.index.render(null, null, null, null);
         assertThat(contentType(html)).isEqualTo("text/html");
         assertThat(contentAsString(html)).contains("iWant2Exchange");
         assertThat(contentAsString(html)).contains("Convert");
+    }
+    @Test
+    public void renderTemplateWithValues() {
+        Content html = views.html.index.render(Currency.getInstance("EUR"), new BigDecimal(100), Currency.getInstance("JPY"), new BigDecimal(200));
+        assertThat(contentType(html)).isEqualTo("text/html");
+        assertThat(contentAsString(html)).contains("EUR");
+        assertThat(contentAsString(html)).contains("100");
+        assertThat(contentAsString(html)).contains("JPY");
+        assertThat(contentAsString(html)).contains("200");
     }
 }
